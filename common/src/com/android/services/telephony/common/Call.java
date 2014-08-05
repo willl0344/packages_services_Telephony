ceaa174c972c552bd673a2251db0a3d6de596ce2
emancebo <emancebo@cyngn.com>
2014-08-05 14:57:39 -0700

    Pass creation time in Call interface so that we can uniquely identify calls in call log
    Change-Id: Id235a1fa3b639ad684b4f69d676e55925886d905

--- common/src/com/android/services/telephony/common/Call.java
+++ common/src/com/android/services/telephony/common/Call.java
@@ -184,6 +184,9 @@ public final class Call implements Parcelable {
     // Time that this call transitioned into ACTIVE state from INCOMING, WAITING, or OUTGOING.
     private long mConnectTime = 0;
 
+    // Time at which the connection object was created
+    private long mCreateTime = 0;
+
     // List of call Ids for for this call.  (Used for managing conference calls).
     private SortedSet<Integer> mChildCallIds = Sets.newSortedSet();
 
@@ -211,6 +214,7 @@ public final class Call implements Parcelable {
         mDisconnectCause = call.mDisconnectCause;
         mCapabilities = call.mCapabilities;
         mConnectTime = call.mConnectTime;
+        mCreateTime = call.mCreateTime;
         mChildCallIds = new TreeSet<Integer>(call.mChildCallIds);
         mGatewayNumber = call.mGatewayNumber;
         mGatewayPackage = call.mGatewayPackage;
@@ -302,6 +306,14 @@ public final class Call implements Parcelable {
         return mConnectTime;
     }
 
+    public void setCreateTime(long createTime) {
+        mCreateTime = createTime;
+    }
+
+    public long getCreateTime() {
+        return mCreateTime;
+    }
+
     public boolean isForwarded() {
         return mForwarded;
     }
@@ -364,6 +376,7 @@ public final class Call implements Parcelable {
         dest.writeInt(mState);
         dest.writeString(getDisconnectCause().toString());
         dest.writeInt(getCapabilities());
+        dest.writeLong(getCreateTime());
         dest.writeLong(getConnectTime());
         dest.writeIntArray(Ints.toArray(mChildCallIds));
         dest.writeString(getGatewayNumber());
@@ -381,6 +394,7 @@ public final class Call implements Parcelable {
         mState = in.readInt();
         mDisconnectCause = DisconnectCause.valueOf(in.readString());
         mCapabilities = in.readInt();
+        mCreateTime = in.readLong();
         mConnectTime = in.readLong();
         mChildCallIds.addAll(Ints.asList(in.createIntArray()));
         mGatewayNumber = in.readString();
@@ -420,6 +434,7 @@ public final class Call implements Parcelable {
                 .add("mDisconnectCause", mDisconnectCause)
                 .add("mCapabilities", mCapabilities)
                 .add("mConnectTime", mConnectTime)
+                .add("mCreateTime", mCreateTime)
                 .add("mChildCallIds", mChildCallIds)
                 .add("mGatewayNumber", MoreStrings.toSafeString(mGatewayNumber))
                 .add("mGatewayPackage", mGatewayPackage)
